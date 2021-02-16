ca8a7fa7b8490b064a68665fc42565e2e6ff79a0
Munjal Mayank <munjaldevelopment@gmail.com>
2021-02-16 14:02:21 +0530

    Update login-jmvd.tpl.php

--- htdocs/core/tpl/login-jmvd.tpl.php
+++ htdocs/core/tpl/login-jmvd.tpl.php
@@ -107,7 +107,7 @@ print "<!--begin::Main-->";
 				<div class="d-flex flex-column-fluid flex-column justify-content-between py-9 px-7 py-lg-13 px-lg-35">
 					<!--begin::Logo-->
 					<a href="#" class="text-center pt-2">
-						<img src="<?php echo $urllogo; ?>" class="max-h-75px" alt="" />
+						<img src="<?php echo $urllogo; ?>" class="max-h-250px" alt="" />
 					</a>
 					<!--end::Logo-->
 					<!--begin::Aside body-->
@@ -349,10 +349,14 @@ print "<!--begin::Main-->";
 		<!--end::Login-->
 	</div>
 
+	<!--begin::Global Config(global config for global JS scripts)-->
+	<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };</script>
+	<!--end::Global Config-->
+
 	<!--begin::Global Theme Bundle(used by all pages)-->
 	<script src="<?php echo DOL_URL_ROOT.'/theme/oblyon'; ?>/js/plugins.bundle.js?v=7.2.0"></script>
-	<script src="<?php echo DOL_URL_ROOT.'/theme/oblyon'; ?>/prismjs.bundle.js?v=7.2.0"></script>
-	<script src="<?php echo DOL_URL_ROOT.'/theme/oblyon'; ?>/scripts.bundle.js?v=7.2.0"></script>
+	<script src="<?php echo DOL_URL_ROOT.'/theme/oblyon'; ?>/js/prismjs.bundle.js?v=7.2.0"></script>
+	<script src="<?php echo DOL_URL_ROOT.'/theme/oblyon'; ?>/js/scripts.bundle.js?v=7.2.0"></script>
 	<!--end::Global Theme Bundle-->
 
 	</body>
