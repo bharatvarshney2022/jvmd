f8ea60eb34417b151e571a7216e99ff753b053b4
Munjal Mayank <munjaldevelopment@gmail.com>
2021-02-17 17:26:01 +0530

    Update main.inc.php

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -2554,8 +2554,8 @@ function top_menu_layout($head, $title = '', $target = '', $disablejs = 0, $disa
 										print '									<!--begin::User-->
 										<div class="topbar-item">
 											<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-												<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-												<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">'.$user->login.'</span>
+												<span class="text-muted font-weight-bold font-size-base d-md-inline mr-1">Hi,</span>
+												<span class="text-dark-50 font-weight-bolder font-size-base d-md-inline mr-3">'.$user->login.'</span>
 												<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
 													<span class="symbol-label font-size-h5 font-weight-bold">'.substr(strtoupper($user->login),0,1).'</span>
 												</span>
