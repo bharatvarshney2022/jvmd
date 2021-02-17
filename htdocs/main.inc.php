54f2e4d6b10cbb0ecf3245382db12d0c8b4a1d7b
Munjal Mayank <munjaldevelopment@gmail.com>
2021-02-17 14:51:33 +0530

    top header changes

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -1248,7 +1248,7 @@ if (!function_exists("llxHeaderLayout"))
 			<!--begin::Header-->
 			<div id="kt_header" class="header header-fixed">
 				<!--begin::Container-->
-				<div class="container-fluid d-flex align-items-stretch justify-content-between">
+				<div class="container-fluid no-padding d-flex align-items-stretch justify-content-between">
 					<!--begin::Header Menu Wrapper-->
 					<div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper">
 						<!--begin::Header Menu-->';
@@ -2383,7 +2383,7 @@ function top_htmlhead_layout($head, $title = '', $disablejs = 0, $disablehead = 
  */
 function top_menu_layout($head, $title = '', $target = '', $disablejs = 0, $disablehead = 0, $arrayofjs = '', $arrayofcss = '', $morequerystring = '', $helppagename = '')
 {
-	global $user, $conf, $langs, $db;
+	global $user, $conf, $langs, $db, $mysoc;
 	global $dolibarr_main_authentication, $dolibarr_main_demo;
 	global $hookmanager, $menumanager;
 
@@ -2413,9 +2413,36 @@ function top_menu_layout($head, $title = '', $target = '', $disablejs = 0, $disa
 			$form = new Form($db);
 		}
 
+		$mysoc->logo_squarred_mini=(empty($conf->global->MAIN_INFO_SOCIETE_LOGO_SQUARRED_MINI)?'':$conf->global->MAIN_INFO_SOCIETE_LOGO_SQUARRED_MINI);
+		if (! empty($mysoc->logo_squarred_mini) && is_readable($conf->mycompany->dir_output.'/logos/thumbs/'.$mysoc->logo_squarred_mini))
+        {
+            $urllogo=DOL_URL_ROOT.'/viewimage.php?cache=1&amp;modulepart=mycompany&amp;file='.urlencode('logos/thumbs/'.$mysoc->logo_squarred_mini);
+        }
+        /*elseif (! empty($mysoc->logo_mini) && is_readable($conf->mycompany->dir_output.'/logos/thumbs/'.$mysoc->logo_mini))
+        {
+            $urllogo=DOL_URL_ROOT.'/viewimage.php?cache=1&amp;modulepart=mycompany&amp;file='.urlencode('logos/thumbs/'.$mysoc->logo_mini);
+        }*/
+        else
+        {
+            $urllogo=DOL_URL_ROOT.'/theme/dolibarr_logo.png';
+        }
+
 		//print "\n".'<!-- Start top horizontal -->'."\n";
 
 		print "\n".'										<div id="kt_header_menu" class="header-menu header-menu-mobile header-menu-layout-default'.(GETPOST('dol_invisible_topmenu', 'int') ? ' hidden' : '').'">
+											<!--begin::Brand-->
+											<div class="brand flex-column-auto" id="kt_brand">
+												<!--begin::Logo-->';
+
+								print "\n".'						<a href="'.$dolibarr_main_url_root.'" class="brand-logo white-background">
+													<img alt="Logo" src="'.$urllogo.'" />													
+												</a>
+												<!--end::Logo-->
+												&nbsp;&nbsp;&nbsp; '. $conf->global->MAIN_INFO_SOCIETE_NOM .'
+												
+											</div>
+											<!--end::Brand-->
+
 											<!--begin::Header Nav-->
 											<ul class="menu-nav">';
 		// dol_invisible_topmenu differs from dol_hide_topmenu: dol_invisible_topmenu means we output menu but we make it invisible.
@@ -3490,30 +3517,7 @@ function left_menu_layout($menu_array_before, $helppagename = '', $notused = '',
         }
 
 		print "\n".'				<div class="aside aside-left d-flex flex-column flex-row-auto" id="kt_aside">
-					<!--begin::Brand-->
-					<div class="brand flex-column-auto" id="kt_brand">
-						<!--begin::Logo-->';
-
-		print "\n".'						<a href="'.$dolibarr_main_url_root.'" class="brand-logo white-background">
-							<img alt="Logo" src="'.$urllogo.'" />
-						</a>
-						<!--end::Logo-->
-						<!--begin::Toggle-->
-						<button class="brand-toggle btn btn-sm px-0" id="kt_aside_toggle">
-							<span class="svg-icon svg-icon svg-icon-xl">
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-										<polygon points="0 0 24 0 24 24 0 24" />
-										<path d="M5.29288961,6.70710318 C4.90236532,6.31657888 4.90236532,5.68341391 5.29288961,5.29288961 C5.68341391,4.90236532 6.31657888,4.90236532 6.70710318,5.29288961 L12.7071032,11.2928896 C13.0856821,11.6714686 13.0989277,12.281055 12.7371505,12.675721 L7.23715054,18.675721 C6.86395813,19.08284 6.23139076,19.1103429 5.82427177,18.7371505 C5.41715278,18.3639581 5.38964985,17.7313908 5.76284226,17.3242718 L10.6158586,12.0300721 L5.29288961,6.70710318 Z" fill="#000000" fill-rule="nonzero" transform="translate(8.999997, 11.999999) scale(-1, 1) translate(-8.999997, -11.999999)" />
-										<path d="M10.7071009,15.7071068 C10.3165766,16.0976311 9.68341162,16.0976311 9.29288733,15.7071068 C8.90236304,15.3165825 8.90236304,14.6834175 9.29288733,14.2928932 L15.2928873,8.29289322 C15.6714663,7.91431428 16.2810527,7.90106866 16.6757187,8.26284586 L22.6757187,13.7628459 C23.0828377,14.1360383 23.1103407,14.7686056 22.7371482,15.1757246 C22.3639558,15.5828436 21.7313885,15.6103465 21.3242695,15.2371541 L16.0300699,10.3841378 L10.7071009,15.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" transform="translate(15.999997, 11.999999) scale(-1, 1) rotate(-270.000000) translate(-15.999997, -11.999999)" />
-									</g>
-								</svg>
-								<!--end::Svg Icon-->
-							</span>
-						</button>
-						<!--end::Toolbar-->
-					</div>
-					<!--end::Brand-->';
+					';
 
 		if ($conf->browser->layout == 'phone') $conf->global->MAIN_USE_OLD_SEARCH_FORM = 1; // Select into select2 is awfull on smartphone. TODO Is this still true with select2 v4 ?
 
@@ -3565,7 +3569,24 @@ function left_menu_layout($menu_array_before, $helppagename = '', $notused = '',
 
 		print '					<div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
 						<!--begin::Menu Container-->
-						<div id="kt_aside_menu" class="aside-menu my-4" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500">
+							<div id="kt_aside_menu" class="aside-menu" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500">
+								<!--begin::Toggle-->
+								<div class="brand flex-column-auto"
+								<button class="brand-toggle btn btn-sm px-0" id="kt_aside_toggle">
+									<span class="svg-icon svg-icon svg-icon-xl">
+										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+											<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+												<polygon points="0 0 24 0 24 24 0 24" />
+												<path d="M5.29288961,6.70710318 C4.90236532,6.31657888 4.90236532,5.68341391 5.29288961,5.29288961 C5.68341391,4.90236532 6.31657888,4.90236532 6.70710318,5.29288961 L12.7071032,11.2928896 C13.0856821,11.6714686 13.0989277,12.281055 12.7371505,12.675721 L7.23715054,18.675721 C6.86395813,19.08284 6.23139076,19.1103429 5.82427177,18.7371505 C5.41715278,18.3639581 5.38964985,17.7313908 5.76284226,17.3242718 L10.6158586,12.0300721 L5.29288961,6.70710318 Z" fill="#000000" fill-rule="nonzero" transform="translate(8.999997, 11.999999) scale(-1, 1) translate(-8.999997, -11.999999)" />
+												<path d="M10.7071009,15.7071068 C10.3165766,16.0976311 9.68341162,16.0976311 9.29288733,15.7071068 C8.90236304,15.3165825 8.90236304,14.6834175 9.29288733,14.2928932 L15.2928873,8.29289322 C15.6714663,7.91431428 16.2810527,7.90106866 16.6757187,8.26284586 L22.6757187,13.7628459 C23.0828377,14.1360383 23.1103407,14.7686056 22.7371482,15.1757246 C22.3639558,15.5828436 21.7313885,15.6103465 21.3242695,15.2371541 L16.0300699,10.3841378 L10.7071009,15.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" transform="translate(15.999997, 11.999999) scale(-1, 1) rotate(-270.000000) translate(-15.999997, -11.999999)" />
+											</g>
+										</svg>
+										<!--end::Svg Icon-->
+									</span>
+								</button>
+								</div>
+								<!--end::Toolbar-->
+
 							<!--begin::Menu Nav-->'."\n";
 
 		// Show left menu with other forms
