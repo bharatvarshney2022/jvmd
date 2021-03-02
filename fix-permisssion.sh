a9e5d868de83331a8baadf6e04cab4c266303ed8
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-02 14:02:19 +0530

    permssion

---
+++ fix-permisssion.sh
@@ -0,0 +1,2 @@
+find /home/servejm4/public_html -type d -exec chmod 0755 {} \;
+find /home/servejm4/public_html -type f -exec chmod 0644 {} \;
--- live/fix-permisssion.sh
+++
@@ -1,2 +0,0 @@
-find /home/krishi55/public_html -type d -exec chmod 0755 {} \;
-find /home/krishi55/public_html -type f -exec chmod 0644 {} \;
