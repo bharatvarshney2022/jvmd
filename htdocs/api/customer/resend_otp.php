3845bbb6107a404ec1e792c45c63cd067a38cecf
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-03 13:43:30 +0530

    Update resend_otp.php

--- htdocs/api/customer/resend_otp.php
+++ htdocs/api/customer/resend_otp.php
@@ -21,8 +21,6 @@
 	$isExist = $object->getOTPFromMobile($mobile);
 	if($isExist)
 	{
-		echo '<pre>';print_r($isExist); exit;
-
 		$status_code = '1';
 		$message = 'Resend OTP';
 
@@ -33,7 +31,7 @@
 		$updateSql.= " WHERE phone_mobile = '".(int)$mobile."' ";
 		$resql = $db->query($updateSql);
 
-		$smsmessage = str_replace(" ", "%20", "Dear ".$v->firstname." ".$isExist->lastname.", Your OTP is ".$otp.". Please DO NOT share OTP.");
+		$smsmessage = str_replace(" ", "%20", "Dear ".$isExist['firstname']." ".$isExist['lastname'].", Your OTP is ".$otp.". Please DO NOT share OTP.");
 		$SENDERID = $conf->global->MAIN_MAIL_SMS_FROM;
 		$PHONE = $mobile;
 		$MESSAGE = $smsmessage;
