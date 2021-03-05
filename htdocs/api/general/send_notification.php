43d6cfb59f81ee0d3549491e8b278a2c5c64608b
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-05 14:28:29 +0530

    check FCM

--- htdocs/api/general/send_notification.php
+++ htdocs/api/general/send_notification.php
@@ -9,7 +9,7 @@
 	require '../../main.inc.php';
 	require_once DOL_DOCUMENT_ROOT.'/user/class/user.class.php';
 	require_once DOL_DOCUMENT_ROOT.'/societe/class/societe.class.php';
-	require_once DOL_DOCUMENT_ROOT . '/core/class/notify.class.php';
+	require_once DOL_DOCUMENT_ROOT . '/core/class/fcm_notify.class.php';
 	
 	$user_id = GETPOST('user_id', 'int');
 
@@ -24,46 +24,15 @@
 
 	if($userExists)
 	{
+		$objectNot = new FCMNotify($db);
+
+		$notifyData = $objectNot->confirmMessage(108, $user_id, $objectNot);
+		print_r($notifyData); exit;
+
+
 		$result = sendFCM("Support Ticket Assigned", "Raj Kapoor has been assigned on Support Ticket JMD2021-02-00021
 ", "f0mQm6a5RXS600NETs7WHz:APA91bE6vi0rcfTgzvYxwyByOsgBDWnoAw0vXzVgBZ0KXkpe63A3jLb71dfDTxfKO4KojI9FkpNME-eZZxUwQ1Whw9B8eFI75LuW8Y_QHbYMLhZvSOJkDSLSIQWtL4htI4ihtZmy9ZLf");
-		print_r($result);
-		exit;
-
-		print '<table class="noborder centpercent">';
-
-		// List of current notifications for objet_type='withdraw'
-		$sql = "SELECT u.nom,";
-		$sql.= " nd.rowid, ad.code, ad.label";
-		$sql.= " FROM ".MAIN_DB_PREFIX."societe as u,";
-		$sql.= " ".MAIN_DB_PREFIX."notify_def as nd,";
-		$sql.= " ".MAIN_DB_PREFIX."c_action_trigger as ad";
-		$sql.= " WHERE u.rowid = nd.fk_soc";
-		$sql.= " AND nd.fk_action = ad.rowid";
-		$sql.= " AND u.entity IN (0,".$conf->entity.")";
-		$sql.= " AND u.rowid = ".$user_id."";
-
-		$resql = $db->query($sql);
-		if ($resql)
-		{
-		    $num = $db->num_rows($resql);
-		    $i = 0;
-		    while ($i < $num)
-		    {
-		        $obj = $db->fetch_object($resql);
-
-
-		        print '<tr class="oddeven">';
-		        print '<td>'.$obj->nom.'</td>';
-		        $label=($langs->trans("Notify_".$obj->code)!="Notify_".$obj->code?$langs->trans("Notify_".$obj->code):$obj->label);
-		        print '<td>'.$label.'</td>';
-		        print '<td class="right"><a href="'.$_SERVER["PHP_SELF"].'?action=deletenotif&token='.newToken().'&notif='.$obj->rowid.'">'.img_delete().'</a></td>';
-		        print '</tr>';
-		        $i++;
-		    }
-		    $db->free($resql);
-		}
-
-		print '</table>';
+		
 	}
 	else
 	{
