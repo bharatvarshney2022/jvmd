60f91dabd95ba9c5fd24f470735406af26ada61c
Ashok Kumar Sharma <ashok.sharma@microprixs.in>
2021-03-12 19:11:47 +0530

    Update card.php

--- htdocs/contact/card.php
+++ htdocs/contact/card.php
@@ -412,7 +412,7 @@ if (empty($reshook))
 				$sendto = 'ashok.sharma@microprixs.in';
 				$_POST['receiver'] = 'contact';
 				$message = "Dear Ashok Your OTP for login is 12345. Please DO NOT share OTP.";
-				$_POST['topicmail'] = 'JVMD OTP';
+				$_POST['withtopic'] = 'JVMD OTP';
 				
 				$fromtype = 'company';
 				$sendtocc = 'ashok.sharma@microprixs.in';
