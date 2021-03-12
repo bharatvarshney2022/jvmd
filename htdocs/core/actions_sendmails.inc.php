1184ee27aeba2b8bee4aad60cc2c87dea59ce3b7
Ashok Kumar Sharma <ashok.sharma@microprixs.in>
2021-03-12 19:37:22 +0530

    Update actions_sendmails.inc.php

--- htdocs/core/actions_sendmails.inc.php
+++ htdocs/core/actions_sendmails.inc.php
@@ -468,8 +468,10 @@ if (($action == 'send' || $action == 'relance') && !$_POST['addfile'] && !$_POST
 
   					$moreparam = '';
 	  				if (isset($paramname2) || isset($paramval2)) $moreparam .= '&'.($paramname2 ? $paramname2 : 'mid').'='.$paramval2;
-		  			/*header('Location: '.$_SERVER["PHP_SELF"].'?'.($paramname ? $paramname : 'id').'='.(is_object($object) ? $object->id : '').$moreparam);
-			  		exit;*/
+		  			if($paramname != ''){
+			  			header('Location: '.$_SERVER["PHP_SELF"].'?'.($paramname ? $paramname : 'id').'='.(is_object($object) ? $object->id : '').$moreparam);
+				  		exit;
+				  	}
 				} else {
 					$langs->load("other");
 					$mesg = '<div class="error">';
