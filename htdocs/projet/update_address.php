cc99e7932f51463a4cef8cc9ca1fe08beda04d39
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-12 14:31:43 +0530

    update address

---
+++ htdocs/projet/update_address.php
@@ -0,0 +1,55 @@
+<?php
+/* This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	\file       htdocs/projet/list.php
+ *	\ingroup    projet
+ *	\brief      Page to list projects
+ */
+
+require '../main.inc.php';
+require_once DOL_DOCUMENT_ROOT.'/projet/class/project.class.php';
+
+// Load translation files required by the page
+$langs->loadLangs(array('projects', 'companies', 'commercial'));
+
+// Initialize technical object to manage hooks of page. Note that conf->hooks_modules contains array of hook context
+$object = new Project($db);
+
+$sqlProjet = "SELECT rowid, fk_soc FROM ".MAIN_DB_PREFIX."projet WHERE fk_soc IS NOT NULL";
+$resqlProjet = $db->query($sqlProjet);
+$numProjet = $db->num_rows($resqlProjet);
+if($numProjet > 0){
+	while($projObj = $db->fetch_array($resqlProjet))
+	{
+		//print_r($projObj); exit;
+		$proj_id = $projObj[0];
+		$soc_id = $projObj[1];
+
+		$sqlProjet1 = "SELECT * FROM ".MAIN_DB_PREFIX."societe WHERE rowid = '".$soc_id."'";
+		$resqlProjet1 = $db->query($sqlProjet1);
+		$projObj1 = $db->fetch_array($resqlProjet1);
+
+		$address = $projObj1['address'];
+		$zip = $projObj1['zip'];
+		$town = $projObj1['town'];
+		$fk_departement = $projObj1['fk_departement'];
+		$fk_pays = $projObj1['fk_pays'];
+
+
+		$sqlProjet2 = "UPDATE ".MAIN_DB_PREFIX."projet SET address = '".$db->escape($address)."', zip = '".$db->escape($zip)."', town = '".$db->escape($town)."', fk_departement = '".$db->escape($fk_departement)."', fk_pays = '".$db->escape($fk_pays)."' WHERE rowid = '".$proj_id."'";
+		$db->query($sqlProjet2);
+	}
+}
